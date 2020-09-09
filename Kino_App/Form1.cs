58979c2b3fd1d633a8bb64af3a74c9009661ce3f
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2020-09-09 12:00:55 +0300

    Add project files.

---
+++ Kino_App/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kino_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Label[,] _arr = new Label[4, 4];
+        Label[] read = new Label[4];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                read[i] = new Label();
+                read[i].Text = "Rida " + (i+1);
+                read[i].Size = new Size(50, 50);
+                read[i].Location = new Point(1, i * 50);
+                this.Controls.Add(read[i]);
+                for (int j = 0; j < 4; j++)
+                {     
+                    _arr[i, j] = new Label();
+                    _arr[i, j].Text =  " Koht" + (j+1);//"Rida " + i +
+                    _arr[i, j].Size = new Size(50, 50);
+                    _arr[i, j].Location = new Point(j * 50 + 50, i * 50);                
+                    this.Controls.Add(_arr[i, j]);
+                    _arr[i, j].Tag = new int[] { i, j };
+                    _arr[i, j].Click += new System.EventHandler(Form1_Click);
+
+                }
+            }
+        }
+        void Form1_Click(object sender, EventArgs e)
+        {
+            var label = (Label)sender;//запомникли на какую надпись нажали
+            var tag = (int[])label.Tag;//определили координаты надписи
+            //MessageBox.Show("Kas oled kindel?", "Appolo küsib", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            _arr[tag[0], tag[1]].Text = " See koht on ostetud";
+   
+            
+        }
+    }
+}
